8da6c53e036005e31011defcff124c17f01ee5a6
Alexey <alexey.severyn@gmail.com>
2022-03-17 12:04:02 +0300

    Delete function for todo

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -31,4 +31,7 @@ urlpatterns = [
     path('current/', views.currenttodos, name='currenttodos'),
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+
+    #del
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
