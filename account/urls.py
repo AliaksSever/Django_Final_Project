b9d947ea4024ddb04fae257d21c866a8080f1001
AliaksSever <alexey.severyn@gmail.com>
2022-04-07 16:05:58 +0300

    All the functionality for acc/personal information has been added

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from .views import account_view, account
+from .views import account_view, account, account_update, account_delete
 
 
 urlpatterns = [
     path('', account_view, name='account_view'),
     path('account/', account, name='account'),
+    path('update/', account_update, name='account_update'),
+    path('delete/', account_delete, name='account_delete')
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from urllib import request
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Account
 from .forms import AccountForm
 
@@ -17,7 +18,7 @@ def account_view(request):
             info = form.save(commit=False)
             info.user = request.user
             info.save()
-            return redirect('currenttodos')
+            return redirect('account')
         except ValueError:
             return render(request, 'account/account_view.html', {'form':AccountForm(), 'error':error})
 
@@ -25,6 +26,54 @@ def account_view(request):
 def account(request):
     account = Account.objects.filter(user=request.user)
     return render(request, 'account/account.html', {'account': account})
+
+
+
+def account_update(request): # working 
+    acc = get_object_or_404(Account, user = request.user)
+    error = 'Bad info'
+    if request.method == 'GET':
+        form = AccountForm(instance=acc)
+        return render(request, 'account/account_update.html', {'acc':acc, 'form': form})
+    else:
+        try:
+            form = AccountForm(request.POST, instance=acc)
+            form.save()
+            return redirect('account')
+        except ValueError:
+            return render(request, 'account/account_update.html', {'acc':acc, 'form': form, 'error': error})
+
+
+def account_delete(request):
+    acc = get_object_or_404(Account, user = request.user)
+    if request.method == 'GET':
+        return render(request, 'account/account_delete.html', {'acc': acc})
+    else:
+        acc.delete()
+        return redirect('account')
+
+
+# def tag_delete(request, slug):
+#     tag = get_object_or_404(Tag, slug__iexact=slug, user= request.user)
+#     if request.method == 'GET':
+#         return render(request, 'todo/tag_delete.html', {'tag':tag})
+#     else:
+#         tag.delete()
+#         return redirect('tags')
+
+# @login_required
+# def tag_update(request, slug):
+#     tag = get_object_or_404(Tag, slug__iexact=slug, user= request.user)
+#     if request.method == 'GET':
+#         form = TagForm(instance=tag)
+#         return render(request, 'todo/tag_update.html', {'tag':tag, 'form':form})
+#     else:
+#         try:
+#             form = TagForm(request.POST, instance=tag)
+#             form.save()
+#             return redirect('tags')
+#         except ValueError:
+#             return render(request, 'todo/tag_update.html', {'tag': tag, 'form': form})
 
 # def tags(request):
 #     tags = Tag.objects.filter(user=request.user)
